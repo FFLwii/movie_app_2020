7a6353ba9c9cc57c910ba5421cfa06d401a7f242
wi <hyeonseungw@wiui-MacBookPro.local>
2020-05-01 04:43:03 +0900

    #05.01

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
     //console.log(movies.data.data.movies);//movies -> keep use "var's name" by =this.movie
     //this.setState({movies:movies}); //this.movies ->setState =movies
     this.setState({ movies, isLoading: false });
